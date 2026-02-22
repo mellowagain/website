076975d4c65e1206ca44acf7d73171e5de9b5213
Mari <me@mari.zip>
2026-02-22 20:49:10 +0100

    switch favicon and home image

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,10 @@ export const metadata: Metadata = {
     title: "mari.zip",
     description: "strangely quiet here without you",
     icons: {
-        icon: [
-            {
-                url: "/icon-light-32x32.png",
-                media: "(prefers-color-scheme: light)",
-            },
-            {
-                url: "/icon-dark-32x32.png",
-                media: "(prefers-color-scheme: dark)",
-            },
-            {
-                url: "/icon.svg",
-                type: "image/svg+xml",
-            },
-        ],
-        apple: "/apple-icon.png",
+        icon: {
+            url: "/icon.svg",
+            type: "image/svg+xml",
+        },
     },
 };
 
